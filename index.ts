e385f9610f4effcd065fd2ef9e31005496cf5de3
dylan <dylanbradshaw107@hotmail.com>
2024-12-01 11:13:39 +1300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 80 });
+
+wss.on('connection', function connection(ws) {
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data) {
+        // Broadcast to all clients
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(`Broadcast message: ${data}`);
+            }
+        });
+    });
+});
