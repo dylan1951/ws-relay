38b8a861318a1fd0e77b77ca3278d4ea49fda934
dylan <dylanbradshaw107@hotmail.com>
2024-12-09 21:59:34 +1300

    https

--- server.ts
+++ server.ts
@@ -2,10 +2,19 @@ import Fastify from 'fastify'
 import { FastifySSEPlugin } from 'fastify-sse-v2';
 import { EventEmitter, on } from 'events';
 import ingestRoutes from './routes/ingest';
+import fs from 'fs';
 
 const eventEmitter = new EventEmitter();
 
-const server = Fastify({ logger: true, http2: true });
+const server = Fastify({
+    logger: true,
+    http2: true,
+    https: {
+        allowHTTP1: true,
+        key: fs.readFileSync(`/etc/letsencrypt/live/api.bot.thenano.wiki/privkey.pem`),
+        cert: fs.readFileSync(`/etc/letsencrypt/live/api.bot.thenano.wiki/fullchain.pem`)
+    }
+});
 
 server.register(FastifySSEPlugin);
 server.register(ingestRoutes, { prefix: '/ingest', eventEmitter });
@@ -18,6 +27,10 @@ server.get('/events', (req, res) => {
             }
         })()
     );
+
+    req.socket.on('close', () => {
+       // do cleanup
+    });
 });
 
 server.listen({ port: 80, host: '0.0.0.0' }, (err, address) => {
